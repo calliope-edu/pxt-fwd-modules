92ef17d34f79f8b5e0b6c44e507ccd37ced6f024
Jørn Alraun <joern.alraun@calliope.cc>
2025-10-31 15:45:45 +0100

    Update localization for EC reading methods

--- fwd-ec/client.ts
+++ fwd-ec/client.ts
@@ -7,25 +7,23 @@ namespace fwdSensors {
         constructor(role: string) {
             super(role)
         }
-
         /**
          * Returns the sensor's relative EC reading.
          */
-        //% group="Relative EC"
-        //% block="$this reading"
+        //% group="Relative EC" group.loc.de="Relative LF" group.loc.it="EC Relativa" group.loc.es="CE Relativa" group.loc.el="Σχετική ΗΑ"
+        //% block="$this reading" block.loc.de="$this Messwert" block.loc.it="$this lettura" block.loc.es="$this lectura" block.loc.el="$this ανάγνωση"
         //% blockId=fwd_ec_get_ec
         //% weight=100
         ec(): number {
             return Math.round(super.reading())
         }
-
         /**
          * Returns true when the relative EC is past the provided threshold in the designated direction.
          * @param threshold the EC value to check against
          * @param direction over or under the threshold
          */
-        //% group="Relative EC"
-        //% block="$this is $direction $threshold"
+        //% group="Relative EC" group.loc.de="Relative LF" group.loc.it="EC Relativa" group.loc.es="CE Relativa" group.loc.el="Σχετική ΗΑ"
+        //% block="$this is $direction $threshold" block.loc.de="$this ist $direction $threshold" block.loc.it="$this è $direction $threshold" block.loc.es="$this está $direction $threshold" block.loc.el="$this είναι $direction $threshold"
         //% blockId=fwd_ec_is_past_threshold
         //% weight=99
         isPastThreshold(
@@ -39,7 +37,6 @@ namespace fwdSensors {
             return isPastThreshold
         }
     }
-
     //% fixedInstance whenUsed
     export const ec1 = new FwdEcClient("ec1")
     //% fixedInstance whenUsed
