106ca549084ddd03d6e5a5170c405c1f56aece14
Jørn Alraun <joern.alraun@calliope.cc>
2025-11-05 12:44:47 +0100

    Add localization support for light controls

--- fwd-lights/client.ts
+++ fwd-lights/client.ts
@@ -14,8 +14,18 @@ namespace fwdLights {
          * @param brightness 0%-100%
          */
         //% group="Lights"
+        //% group.loc.de="Lichter"
+        //% group.loc.fr="Lumières"
+        //% group.loc.it="Luci"
+        //% group.loc.es="Luces"
+        //% group.loc.el="Φώτα"
         //% blockId=fwd_lights_set_brightness
         //% block="set $this brightness to $brightness (\\%)"
+        //% block.loc.de="setze $this Helligkeit auf $brightness (\\%)"
+        //% block.loc.fr="règle $this luminosité à $brightness (\\%)"
+        //% block.loc.it="imposta $this luminosità a $brightness (\\%)"
+        //% block.loc.es="establecer $this brillo a $brightness (\\%)"
+        //% block.loc.el="όρισε $this φωτεινότητα σε $brightness (\\%)"
         //% brightness.min=0
         //% brightness.max=100
         //% weight=100
@@ -27,7 +37,17 @@ namespace fwdLights {
          * Returns true if the light brightness is anything other than 0%.
          */
         //% group="Lights"
+        //% group.loc.de="Lichter"
+        //% group.loc.fr="Lumières"
+        //% group.loc.it="Luci"
+        //% group.loc.es="Luces"
+        //% group.loc.el="Φώτα"
         //% block="$this is on"
+        //% block.loc.de="$this ist an"
+        //% block.loc.fr="$this est allumé"
+        //% block.loc.it="$this è acceso"
+        //% block.loc.es="$this está encendido"
+        //% block.loc.el="$this είναι ενεργό"
         //% blockId=fwd_lights_is_on
         //% weight=99
         isOn(): boolean {
