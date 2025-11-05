cb101c0ead750870c20374ee5321e1011d959dfb
Jørn Alraun <joern.alraun@calliope.cc>
2025-11-05 12:49:18 +0100

    Refactor localization comments in client.ts
    Updated localization comments for pump control methods.

--- fwd-pump/client.ts
+++ fwd-pump/client.ts
@@ -7,25 +7,19 @@ namespace fwdMotors {
 
         /**
          * Turn the pump on or off.
-         * @param on on = true, off = false
-         * @de Schaltet die Pumpe ein oder aus.
-         * @fr Allume ou éteint la pompe.
-         * @it Accende o spegne la pompa.
-         * @es Enciende o apaga la bomba.
-         * @el Ενεργοποιεί ή απενεργοποιεί την αντλία.
          */
         //% group="Pump"
-        //% group.de="Pumpe"
-        //% group.fr="Pompe"
-        //% group.it="Pompa"
-        //% group.es="Bomba"
-        //% group.el="Αντλία"
+        //% group.loc.de="Pumpe"
+        //% group.loc.fr="Pompe"
+        //% group.loc.it="Pompa"
+        //% group.loc.es="Bomba"
+        //% group.loc.el="Αντλία"
         //% block="turn $this $on"
-        //% block.de="schalte $this $on"
-        //% block.fr="allumer $this $on"
-        //% block.it="accendi $this $on"
-        //% block.es="encender $this $on"
-        //% block.el="ενεργοποίησε $this $on"
+        //% block.loc.de="schalte $this $on"
+        //% block.loc.fr="allumer $this $on"
+        //% block.loc.it="accendi $this $on"
+        //% block.loc.es="encender $this $on"
+        //% block.loc.el="ενεργοποίησε $this $on"
         //% blockId=fwd_pump_set_on
         //% on.shadow="toggleOnOff"
         //% weight=100
@@ -35,25 +29,19 @@ namespace fwdMotors {
 
         /**
          * Turn on the pump for the selected duration (ms).
-         * @param duration how long to run the pump (ms)
-         * @de Schaltet die Pumpe für die angegebene Dauer (ms) ein.
-         * @fr Allume la pompe pendant la durée sélectionnée (ms).
-         * @it Accende la pompa per la durata selezionata (ms).
-         * @es Enciende la bomba durante la duración seleccionada (ms).
-         * @el Ενεργοποιεί την αντλία για την επιλεγμένη διάρκεια (ms).
          */
         //% group="Pump"
-        //% group.de="Pumpe"
-        //% group.fr="Pompe"
-        //% group.it="Pompa"
-        //% group.es="Bomba"
-        //% group.el="Αντλία"
+        //% group.loc.de="Pumpe"
+        //% group.loc.fr="Pompe"
+        //% group.loc.it="Pompa"
+        //% group.loc.es="Bomba"
+        //% group.loc.el="Αντλία"
         //% block="run $this for $duration ms"
-        //% block.de="betreibe $this für $duration ms"
-        //% block.fr="faire fonctionner $this pendant $duration ms"
-        //% block.it="avvia $this per $duration ms"
-        //% block.es="ejecutar $this durante $duration ms"
-        //% block.el="εκτέλεσε $this για $duration ms"
+        //% block.loc.de="betreibe $this für $duration ms"
+        //% block.loc.fr="faire fonctionner $this pendant $duration ms"
+        //% block.loc.it="avvia $this per $duration ms"
+        //% block.loc.es="ejecutar $this durante $duration ms"
+        //% block.loc.el="εκτέλεσε $this για $duration ms"
         //% duration.shadow="timePicker"
         //% blockId=fwd_pump_timed_run
         //% weight=99
@@ -66,25 +54,20 @@ namespace fwdMotors {
         }
 
         /**
-         * Returns true if the pump is running and false if it's stopped.
-         * @de Gibt wahr zurück, wenn die Pumpe läuft, andernfalls falsch.
-         * @fr Renvoie vrai si la pompe fonctionne, sinon faux.
-         * @it Restituisce vero se la pompa è in funzione, altrimenti falso.
-         * @es Devuelve verdadero si la bomba está en funcionamiento, falso si está parada.
-         * @el Επιστρέφει αληθές εάν η αντλία λειτουργεί και ψευδές εάν έχει σταματήσει.
+         * Returns true if the pump is running.
          */
         //% group="Pump"
-        //% group.de="Pumpe"
-        //% group.fr="Pompe"
-        //% group.it="Pompa"
-        //% group.es="Bomba"
-        //% group.el="Αντλία"
+        //% group.loc.de="Pumpe"
+        //% group.loc.fr="Pompe"
+        //% group.loc.it="Pompa"
+        //% group.loc.es="Bomba"
+        //% group.loc.el="Αντλία"
         //% block="$this is on"
-        //% block.de="$this ist an"
-        //% block.fr="$this est activée"
-        //% block.it="$this è accesa"
-        //% block.es="$this está encendida"
-        //% block.el="$this είναι ενεργή"
+        //% block.loc.de="$this ist an"
+        //% block.loc.fr="$this est activée"
+        //% block.loc.it="$this è accesa"
+        //% block.loc.es="$this está encendida"
+        //% block.loc.el="$this είναι ενεργή"
         //% blockId=fwd_pump_is_on
         //% weight=98
         isOn(): boolean {
