a5b5d35310deccb0a96d22c9b1736f908cf42d17
Jørn Alraun <joern.alraun@calliope.cc>
2025-11-05 12:42:40 +0100

    Update localization comments for LCD blocks

--- fwd-lcd/client.ts
+++ fwd-lcd/client.ts
@@ -10,11 +10,11 @@ namespace fwdSensors {
      * @el Η αρχικοποίηση κάνει τα μπλοκ LCD πιο αξιόπιστα στο "on start".
      */
     //% block="initialize LCD"
-    //% block.de="LCD initialisieren"
-    //% block.fr="initialiser LCD"
-    //% block.it="inizializza LCD"
-    //% block.es="inicializar LCD"
-    //% block.el="αρχικοποίηση LCD"
+    //% block.loc.de="LCD initialisieren"
+    //% block.loc.fr="initialiser LCD"
+    //% block.loc.it="inizializza LCD"
+    //% block.loc.es="inicializar LCD"
+    //% block.loc.el="αρχικοποίηση LCD"
     //% blockId=fwd_lcd_initialize
     //% group="LCD"
     //% weight=100
@@ -31,11 +31,11 @@ namespace fwdSensors {
      * @el Στρογγυλοποιεί έναν αριθμό στο καθορισμένο δεκαδικό ψηφίο.
      */
     //% block="round $number_ to $decimals decimals"
-    //% block.de="runde $number_ auf $decimals Nachkommastellen"
-    //% block.fr="arrondir $number_ à $decimals décimales"
-    //% block.it="arrotonda $number_ a $decimals decimali"
-    //% block.es="redondear $number_ a $decimals decimales"
-    //% block.el="στρογγυλοποίηση $number_ σε $decimals δεκαδικά"
+    //% block.loc.de="runde $number_ auf $decimals Nachkommastellen"
+    //% block.loc.fr="arrondir $number_ à $decimals décimales"
+    //% block.loc.it="arrotonda $number_ a $decimals decimali"
+    //% block.loc.es="redondear $number_ a $decimals decimales"
+    //% block.loc.el="στρογγυλοποίηση $number_ σε $decimals δεκαδικά"
     //% decimals.min=0 decimals.max=4 decimals.defl=0
     //% blockId=fwd_lcd_round
     //% group="LCD"
@@ -56,11 +56,11 @@ namespace fwdSensors {
         }
 
         //% block="clear $this"
-        //% block.de="$this Bildschirm löschen"
-        //% block.fr="effacer $this"
-        //% block.it="cancella $this"
-        //% block.es="borrar $this"
-        //% block.el="καθαρισμός $this"
+        //% block.loc.de="$this Bildschirm löschen"
+        //% block.loc.fr="effacer $this"
+        //% block.loc.it="cancella $this"
+        //% block.loc.es="borrar $this"
+        //% block.loc.el="καθαρισμός $this"
         //% blockId=fwd_lcd_clear_screen
         //% group="LCD"
         //% weight=94
@@ -70,11 +70,11 @@ namespace fwdSensors {
         }
 
         //% block="print string $string_ on line $line of $this"
-        //% block.de="zeige Text $string_ auf Zeile $line von $this"
-        //% block.fr="afficher texte $string_ sur ligne $line de $this"
-        //% block.it="stampa testo $string_ su riga $line di $this"
-        //% block.es="imprimir texto $string_ en línea $line de $this"
-        //% block.el="εκτύπωση $string_ στη γραμμή $line του $this"
+        //% block.loc.de="zeige Text $string_ auf Zeile $line von $this"
+        //% block.loc.fr="afficher texte $string_ sur ligne $line de $this"
+        //% block.loc.it="stampa testo $string_ su riga $line di $this"
+        //% block.loc.es="imprimir texto $string_ en línea $line de $this"
+        //% block.loc.el="εκτύπωση $string_ στη γραμμή $line του $this"
         //% line.min=1 line.max=2 line.defl=1
         //% blockId=fwd_lcd_print_line_string
         //% group="LCD"
@@ -96,11 +96,11 @@ namespace fwdSensors {
         }
 
         //% block="print string $string_ on quadrant $quadrant of $this"
-        //% block.de="zeige Text $string_ im Quadrant $quadrant von $this"
-        //% block.fr="afficher texte $string_ dans quadrant $quadrant de $this"
-        //% block.it="stampa testo $string_ nel quadrante $quadrant di $this"
-        //% block.es="imprimir texto $string_ en cuadrante $quadrant de $this"
-        //% block.el="εκτύπωση $string_ στο τεταρτημόριο $quadrant του $this"
+        //% block.loc.de="zeige Text $string_ im Quadrant $quadrant von $this"
+        //% block.loc.fr="afficher texte $string_ dans quadrant $quadrant de $this"
+        //% block.loc.it="stampa testo $string_ nel quadrante $quadrant di $this"
+        //% block.loc.es="imprimir texto $string_ en cuadrante $quadrant de $this"
+        //% block.loc.el="εκτύπωση $string_ στο τεταρτημόριο $quadrant του $this"
         //% quadrant.min=1 quadrant.max=4 quadrant.defl=1
         //% blockId=fwd_lcd_print_quadrant_string
         //% group="LCD"
@@ -113,7 +113,7 @@ namespace fwdSensors {
                 case 4: col = 8; row = 1; rightAlign = true; break
                 case 1: default: break
             }
-            if (![1,2,3,4].includes(quadrant)) {
+            if (![1, 2, 3, 4].includes(quadrant)) {
                 this.setCursorAndWait(col, row)
                 this.printAndWait("err:!1-4", 'quadrant', rightAlign)
                 return
@@ -127,11 +127,11 @@ namespace fwdSensors {
         }
 
         //% block="print number $number_ on line $line of $this"
-        //% block.de="zeige Zahl $number_ auf Zeile $line von $this"
-        //% block.fr="afficher nombre $number_ sur ligne $line de $this"
-        //% block.it="stampa numero $number_ su riga $line di $this"
-        //% block.es="imprimir número $number_ en línea $line de $this"
-        //% block.el="εκτύπωση αριθμού $number_ στη γραμμή $line του $this"
+        //% block.loc.de="zeige Zahl $number_ auf Zeile $line von $this"
+        //% block.loc.fr="afficher nombre $number_ sur ligne $line de $this"
+        //% block.loc.it="stampa numero $number_ su riga $line di $this"
+        //% block.loc.es="imprimir número $number_ en línea $line de $this"
+        //% block.loc.el="εκτύπωση αριθμού $number_ στη γραμμή $line του $this"
         //% line.min=1 line.max=2 line.defl=1
         //% blockId=fwd_lcd_print_line_number
         //% group="LCD"
@@ -141,11 +141,11 @@ namespace fwdSensors {
         }
 
         //% block="print number $number_ on quadrant $quadrant of $this"
-        //% block.de="zeige Zahl $number_ im Quadrant $quadrant von $this"
-        //% block.fr="afficher nombre $number_ dans quadrant $quadrant de $this"
-        //% block.it="stampa numero $number_ nel quadrante $quadrant di $this"
-        //% block.es="imprimir número $number_ en cuadrante $quadrant de $this"
-        //% block.el="εκτύπωση αριθμού $number_ στο τεταρτημόριο $quadrant του $this"
+        //% block.loc.de="zeige Zahl $number_ im Quadrant $quadrant von $this"
+        //% block.loc.fr="afficher nombre $number_ dans quadrant $quadrant de $this"
+        //% block.loc.it="stampa numero $number_ nel quadrante $quadrant di $this"
+        //% block.loc.es="imprimir número $number_ en cuadrante $quadrant de $this"
+        //% block.loc.el="εκτύπωση αριθμού $number_ στο τεταρτημόριο $quadrant του $this"
         //% quadrant.min=1 quadrant.max=4 quadrant.defl=1
         //% blockId=fwd_lcd_print_quadrant_number
         //% group="LCD"
