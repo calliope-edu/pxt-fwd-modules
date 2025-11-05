b9969ae3e5c2f2c8cd1afb87de56db3b2c2bad54
Jørn Alraun <joern.alraun@calliope.cc>
2025-11-05 13:07:57 +0100

    Update localization comments in client.ts

--- fwd-touch/client.ts
+++ fwd-touch/client.ts
@@ -9,24 +9,24 @@ namespace fwdButtons {
          * Code to run when a chosen event occurs
          * @param event button down, hold, or up
          * @param handler the code to run
-         * @de Führt Code aus, wenn ein gewähltes Ereignis auftritt
-         * @fr Exécute du code lorsqu’un événement choisi se produit
-         * @it Esegue codice quando si verifica un evento selezionato
-         * @es Ejecuta código cuando ocurre un evento seleccionado
-         * @el Εκτελεί κώδικα όταν συμβεί το επιλεγμένο γεγονός
+         * @locale de Führt Code aus, wenn ein gewähltes Ereignis auftritt
+         * @locale fr Exécute du code lorsqu’un événement choisi se produit
+         * @locale it Esegue codice quando si verifica un evento selezionato
+         * @locale es Ejecuta código cuando ocurre un evento seleccionado
+         * @locale el Εκτελεί κώδικα όταν συμβεί το επιλεγμένο γεγονός
          */
         //% group="Touch"
-        //% group.de="Berührung"
-        //% group.fr="Tactile"
-        //% group.it="Tatto"
-        //% group.es="Táctil"
-        //% group.el="Αγγίξτε"
+        //% group.loc.de="Berührung"
+        //% group.loc.fr="Tactile"
+        //% group.loc.it="Tatto"
+        //% group.loc.es="Táctil"
+        //% group.loc.el="Αγγίξτε"
         //% block="on $this $event"
-        //% block.de="wenn $this $event"
-        //% block.fr="lors de $this $event"
-        //% block.it="quando $this $event"
-        //% block.es="cuando $this $event"
-        //% block.el="όταν $this $event"
+        //% block.loc.de="wenn $this $event"
+        //% block.loc.fr="lors de $this $event"
+        //% block.loc.it="quando $this $event"
+        //% block.loc.es="cuando $this $event"
+        //% block.loc.el="όταν $this $event"
         //% blockId=fwd_touch_on_event
         //% weight=100
         onEvent(event: jacdac.ButtonEvent, handler: () => void) {
@@ -35,24 +35,24 @@ namespace fwdButtons {
 
         /**
          * Returns the duration (ms) of the button hold in ms
-         * @de Gibt die Haltezeit der Taste in ms zurück
-         * @fr Renvoie la durée de pression du bouton en ms
-         * @it Restituisce la durata della pressione del pulsante in ms
-         * @es Devuelve la duración de la pulsación del botón en ms
-         * @el Επιστρέφει τη διάρκεια πίεσης του κουμπιού σε ms
+         * @locale de Gibt die Haltezeit der Taste in ms zurück
+         * @locale fr Renvoie la durée de pression du bouton en ms
+         * @locale it Restituisce la durata della pressione del pulsante in ms
+         * @locale es Devuelve la duración de la pulsación del botón en ms
+         * @locale el Επιστρέφει τη διάρκεια πίεσης του κουμπιού σε ms
          */
         //% group="Touch"
-        //% group.de="Berührung"
-        //% group.fr="Tactile"
-        //% group.it="Tatto"
-        //% group.es="Táctil"
-        //% group.el="Αγγίξτε"
+        //% group.loc.de="Berührung"
+        //% group.loc.fr="Tactile"
+        //% group.loc.it="Tatto"
+        //% group.loc.es="Táctil"
+        //% group.loc.el="Αγγίξτε"
         //% block="$this hold duration (ms)"
-        //% block.de="$this Haltezeit (ms)"
-        //% block.fr="durée $this (ms)"
-        //% block.it="durata $this (ms)"
-        //% block.es="duración $this (ms)"
-        //% block.el="διάρκεια $this (ms)"
+        //% block.loc.de="$this Haltezeit (ms)"
+        //% block.loc.fr="durée $this (ms)"
+        //% block.loc.it="durata $this (ms)"
+        //% block.loc.es="duración $this (ms)"
+        //% block.loc.el="διάρκεια $this (ms)"
         //% blockId=fwd_touch_hold_duration
         //% weight=99
         holdDuration(): number {
@@ -61,24 +61,24 @@ namespace fwdButtons {
 
         /**
          * Returns true if the button is currently pressed, otherwise false
-         * @de Gibt wahr zurück, wenn die Taste gedrückt ist, sonst falsch
-         * @fr Renvoie vrai si le bouton est pressé, sinon faux
-         * @it Restituisce vero se il pulsante è premuto, altrimenti falso
-         * @es Devuelve verdadero si el botón está presionado, de lo contrario falso
-         * @el Επιστρέφει αληθές αν το κουμπί πατηθεί, αλλιώς ψευδές
+         * @locale de Gibt wahr zurück, wenn die Taste gedrückt ist, sonst falsch
+         * @locale fr Renvoie vrai si le bouton est pressé, sinon faux
+         * @locale it Restituisce vero se il pulsante è premuto, altrimenti falso
+         * @locale es Devuelve verdadero si el botón está presionado, de lo contrario falso
+         * @locale el Επιστρέφει αληθές αν το κουμπί πατηθεί, αλλιώς ψευδές
          */
         //% group="Touch"
-        //% group.de="Berührung"
-        //% group.fr="Tactile"
-        //% group.it="Tatto"
-        //% group.es="Táctil"
-        //% group.el="Αγγίξτε"
+        //% group.loc.de="Berührung"
+        //% group.loc.fr="Tactile"
+        //% group.loc.it="Tatto"
+        //% group.loc.es="Táctil"
+        //% group.loc.el="Αγγίξτε"
         //% block="$this is pressed"
-        //% block.de="$this ist gedrückt"
-        //% block.fr="$this est pressé"
-        //% block.it="$this è premuto"
-        //% block.es="$this está presionado"
-        //% block.el="$this είναι πατημένο"
+        //% block.loc.de="$this ist gedrückt"
+        //% block.loc.fr="$this est pressé"
+        //% block.loc.it="$this è premuto"
+        //% block.loc.es="$this está presionado"
+        //% block.loc.el="$this είναι πατημένο"
         //% blockId=fwd_touch_is_pressed
         //% weight=98
         isPressed(): boolean {
