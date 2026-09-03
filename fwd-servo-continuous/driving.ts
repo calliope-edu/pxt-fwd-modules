d57eba999cccaaf41b6ac469984abc82dbe5cc51
Otto <31897673+AmandoP@users.noreply.github.com>
2026-09-03 14:14:57 +0200

    Refactor driving controls to remove bias handling

--- fwd-servo-continuous/driving.ts
+++ fwd-servo-continuous/driving.ts
@@ -3,55 +3,40 @@ namespace fwdMotors {
         let enabled = false
         let leftMotor: fwdBase.FwdServoClient
         let rightMotor: fwdBase.FwdServoClient
-        let leftBias = 1
-        let rightBias = 1
-
         function initMotors(
             left: fwdBase.FwdServoClient,
-            right: fwdBase.FwdServoClient,
-            bias = 0
+            right: fwdBase.FwdServoClient
         ) {
             leftMotor = left
             rightMotor = right
-            if (bias > 0) {
-                leftBias = (100 - bias) / 100
-            } else {
-                rightBias = (100 - bias) / 100
-            }
             enabled = true
         }
-
         function drive(direction: -1 | 1, speed: number): void {
             if (!enabled) return
-            leftMotor.setSpeed(direction * speed * leftBias)
-            rightMotor.setSpeed(-direction * speed * rightBias)
+            leftMotor.setSpeed(direction * speed)
+            rightMotor.setSpeed(-direction * speed)
         }
-
         function turnInPlace(angle: number): void {
             if (!enabled) return
             const DEG_PER_SEC = 30
             const direction = angle > 0 ? 1 : -1
-            leftMotor.setSpeed(60 * direction * leftBias)
-            rightMotor.setSpeed(60 * direction * rightBias)
+            leftMotor.setSpeed(60 * direction)
+            rightMotor.setSpeed(60 * direction)
             basic.pause((Math.abs(angle) / DEG_PER_SEC) * 1000)
             leftMotor.setSpeed(0)
             rightMotor.setSpeed(0)
         }
-
         return {
             initMotors: initMotors,
             drive: drive,
             turnInPlace: turnInPlace,
         }
     }
-
     export const drivingControls = createDrivingControls()
-
     /**
      * Use this block before using the other driving blocks to set which servo clients get treated as left and right.
      * @param left the servo client to use as the left motor
      * @param right the servo client to use as the right motor
-     * @param bias if the motors don't spin at quite the same rate, use the bias to balance them
      */
     //% group="Driving"
     //% group.de="Fahren"
@@ -59,25 +44,21 @@ namespace fwdMotors {
     //% group.it="Guida"
     //% group.es="Conducción"
     //% group.el="Οδήγηση"
-    //% block="setup driving|left motor $left|right motor $right||left/right bias $bias"
-    //% block.de="Fahren einrichten|linker Motor $left|rechter Motor $right||Links/Rechts Bias $bias"
-    //% block.fr="configurer conduite|moteur gauche $left|moteur droit $right||biais gauche/droite $bias"
-    //% block.it="configura guida|motore sinistro $left|motore destro $right||bias sinistra/destra $bias"
-    //% block.es="configurar conducción|motor izquierdo $left|motor derecho $right||bias izquierda/derecha $bias"
-    //% block.el="ρύθμιση οδήγησης|αριστερός κινητήρας $left|δεξιός κινητήρας $right||διαφορά αριστερά/δεξιά $bias"
+    //% block="setup driving|left motor $left|right motor $right"
+    //% block.de="Fahren einrichten|linker Motor $left|rechter Motor $right"
+    //% block.fr="configurer conduite|moteur gauche $left|moteur droit $right"
+    //% block.it="configura guida|motore sinistro $left|motore destro $right"
+    //% block.es="configurar conducción|motor izquierdo $left|motor derecho $right"
+    //% block.el="ρύθμιση οδήγησης|αριστερός κινητήρας $left|δεξιός κινητήρας $right"
     //% blockId=fwd_driving_setup
-    //% bias.shadow="speedPicker"
-    //% bias.defl=0
     //% inlineInputMode=external
     //% weight=100
     export function setupDriving(
         left: fwdBase.FwdServoClient,
-        right: fwdBase.FwdServoClient,
-        bias = 0
+        right: fwdBase.FwdServoClient
     ) {
-        drivingControls.initMotors(left, right, bias)
+        drivingControls.initMotors(left, right)
     }
-
     /**
      * Drive forwards or backwards at the desired speed (requires setup driving to used first).
      * @param direction forwards or backwards
@@ -101,7 +82,6 @@ namespace fwdMotors {
     export function drive(direction: fwdEnums.ForwardReverse, speed: number) {
         drivingControls.drive(direction, speed)
     }
-
     /**
      * Stop driving or turning. Sets speed to 0%.
      */
@@ -122,7 +102,6 @@ namespace fwdMotors {
     export function stop() {
         drivingControls.drive(1, 0)
     }
-
     /**
      * Turns the vehicle left or right in place by spinning the wheels in opposite directions.
      * @param angle positive angles turn right, negative angles turn left
